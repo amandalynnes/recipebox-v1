6c5d1da0375bf7502b9d70ae76e37d4deb9fa818
Amanda Simmons <carpe737diem@gmail.com>
2021-02-03 15:19:56 -0500

    added recipe and author models to model.py file

--- recipe_app/models.py
+++ recipe_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+"""
+Author model:
+---
+Name (CharField)
+Bio (TextField)
+
+Recipe Model:
+---
+Title (CharField)
+Author (ForeignKey)
+Description (TextField)
+Time Required (Charfield) (for example, "One hour")
+Instructions (TextField)
+"""
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=150)
+    bio = models.TextField()
+
+
+class RecipeItem(models.Model):
+    title = models.CharField(max_length=40)
+    author = ''
+    description = models.TextField()
+    time_required = models.CharField(max_length=100)
+    instructions = models.TextField()
